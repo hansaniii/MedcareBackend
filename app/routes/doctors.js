a8768de29c1a024f1770b62baba7aba6de092fe7
Charles2042 <142148800+Charles2042@users.noreply.github.com>
2023-10-10 22:08:10 +0530

    Update doctors.js

--- app/routes/doctors.js
+++ app/routes/doctors.js
@@ -1,18 +1,16 @@
 const router = require("express").Router();
-let Doctor = require("../models/doctor");
-const passportLocalMongoose = require('passport-local-mongoose');
+const User = require('../database/schemas/doctor');
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-//const crypto = require('crypto');
 const nodemailer = require('nodemailer');
 
 
 // Create a transporter object to send the email
 const transporter = nodemailer.createTransport({
-  service: 'Gmail', //email service 
+  service: 'Gmail', // Use the email service you want to use
   auth: {
-    user: 'medcaremails@gmail.com', // Replaced with our email address
-    pass: 'ppjv mtjw rbzm rrde', // Replaced with our email password
+    user: 'medcaremails@gmail.com', // Replace with your email address
+    pass: 'ppjv mtjw rbzm rrde', // Replace with your email password
   },
 });
 
@@ -25,7 +23,7 @@ function generateOTP() {
 router.post('/docreg', async (req, res) => {
   const {fname, email, conno, slmc, password } = req.body;
   const otp = generateOTP();
-  const otpExpiration = new Date(Date.now() + 1* 60 * 1000); // OTP expires in 1 minutes
+  const otpExpiration = new Date(Date.now() + 1* 60 * 1000); // OTP expires in 5 minutes
 
   try {
 // Save user to the database
@@ -92,6 +90,8 @@ router.post("/resendmail",async(req,res) => {
   try{
     const {fname, email, conno, slmc, password } = req.body;
 
+    
+   
     if (!fname || !email || !conno || !slmc ||!password){
       throw Error("Empty user details are not allowed");
     }else {
@@ -102,7 +102,7 @@ router.post("/resendmail",async(req,res) => {
       const user = new Doctor({fname, email, conno, slmc ,password, otp, otpExpiration });
       await user.save();
       await transporter.sendMail({
-        from:'medcaremails@gmail.com', // Replace with your email address
+        from:'medcaremails@gmail.com', // Sender email address
         to: email,
         subject: 'OTP Verification',
         text: `Enter ${otp} in the application to verify your email address and complete the signup process. This code expires in 10 minutes`,
@@ -119,35 +119,4 @@ router.post("/resendmail",async(req,res) => {
   
 });
 
-// Doctor login
-router.route("/login").post(async (req, res)=>{
-  const { email, password } = req.body;
-
-  try {
-    // Find the doctor user by email
-    const doctor = await Doctor.findOne({ email });
-
-    if (!doctor) {
-      return res.status(404).json({ message: 'Doctor not found.' });
-    }
-
-    if (password !== doctor.password) {
-      return res.status(401).json({ message: 'Invalid password.' });
-    }
-
-    // Generate a JWT token for the doctor
-    const token = jwt.sign({ userId: doctor._id, userType: 'doctor' }, secretKey, {
-    expiresIn: '1h', // Token expires in 1 hour (adjust as needed)
-    });
-
-    // Loging success message
-     return res.status(200).json({message:'Doctor login successful', token});
-
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal server error.' });
-  }
-});
-
 module.exports = router;
-
