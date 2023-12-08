fd7d95d23656d2f7adf53c0c43e3375a92595323
AmandiLakshanii <121819799+AmandiLakshanii@users.noreply.github.com>
2023-12-08 08:14:53 +0530

    Add files via upload

---
+++ authenticate.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+const dotenv = require("dotenv");
+dotenv.config();
+
+
+function protect(req, res, next) {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+
+      //decodes token id
+      const decoded = jwt.verify(token, process.env.JWT_KEY);
+
+      next();
+    } catch (error) {
+      res.status(401);
+      throw new Error("Not authorized, token failed");
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized, no token");
+  }
+}
+
+module.exports = {protect};
